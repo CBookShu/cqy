b9aa6a309c423bba688a0c53597c6a09cb7a5f7d
cbookshu <cbookshu@outlook.com>
2025-03-23 16:54:05 +0800

    deleted:    3rd/yalantinglibs

--- 3rd/yalantinglibs/src/struct_pack/benchmark/no_op.cpp
+++
@@ -1,5 +0,0 @@
-#include "no_op.h"
-
-void no_op(std::string& str) {}
-
-void no_op(char* data) {}
--- 3rd/yalantinglibs/src/struct_pack/benchmark/no_op.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-
-void no_op(std::string& str);
-void no_op(char* data);
