b8c7cd5e4f8dbd67cedc80e53f7b4ac82e999636
cbookshu <cookshu@outlook.com>
2025-04-21 12:11:48 +0800

    modified:   CMakeLists.txt 	renamed:    test/rpc_bench_client.cpp -> test/benchmark/rpc_bench_client.cpp 	renamed:    test/rpc_bench_def.h -> test/benchmark/rpc_bench_def.h 	renamed:    test/rpc_bench_server.cpp -> test/benchmark/rpc_bench_server.cpp

--- test/benchmark/rpc_bench_client.cpp
+++ test/benchmark/rpc_bench_client.cpp
@@ -12,6 +12,12 @@
 
 using namespace cqy;
 
+constexpr unsigned thread_cnt = 1920;
+constexpr auto request_cnt = 100000;
+std::atomic<uint64_t> working_echo = 0;
+std::atomic<int32_t> qps = 0;
+std::vector<std::chrono::microseconds> result;
+
 struct bench_client : public cqy_ctx {
   std::atomic_uint32_t call_count{0};
   virtual bool on_init(std::string_view param) override {
@@ -84,9 +90,75 @@ struct bench_client : public cqy_ctx {
     co_return;
   }
 
+  Lazy<std::vector<std::chrono::microseconds>> send() {
+    std::vector<std::chrono::microseconds> latencys;
+    latencys.reserve(request_cnt);
+    auto tp = std::chrono::steady_clock::now();
+    ++working_echo;
+    int id = 0;
+    for (int i = 0; i < request_cnt; ++i) {
+      auto result = co_await ctx_call_name_nolock<param_t>("n2.bench_server", "rpc_test", param_t{"hello", 1});
+      if (result.has_error()) {
+        ELOG_ERROR << "get client form client pool failed: \n"
+                   << result.status;
+        continue;
+      }
+      qps.fetch_add(1, std::memory_order::release);
+      auto old_tp = tp;
+      tp = std::chrono::steady_clock::now();
+      latencys.push_back(
+          std::chrono::duration_cast<std::chrono::microseconds>(tp - old_tp));
+    }
+    co_return std::move(latencys);
+  }
+  
+
+  Lazy<void> qps_watcher() {
+    using namespace std::chrono_literals;
+    auto n = get_app()->get_node().get_node("n2");
+    do {
+      co_await coro_io::sleep_for(1s);
+      uint64_t cnt = qps.exchange(0);
+      std::cout << "QPS:" << cnt
+                << " free connection: " << n->rpc_client->free_client_count()
+                << " working echo:" << working_echo << std::endl;
+    } while (working_echo > 0);
+  }
+
+  void latency_watcher() {
+    std::sort(result.begin(), result.end());
+    auto arr = {0.1, 0.3, 0.5, 0.7, 0.9, 0.95, 0.99, 0.999, 0.9999, 0.99999, 1.0};
+    for (auto e : arr) {
+      std::cout
+          << (e * 100) << "% request finished in:"
+          << result[std::max<std::size_t>(0, result.size() * e - 1)].count() /
+                 1000.0
+          << "ms" << std::endl;
+    }
+  }
+
+  Lazy<void> test_rpc2() {
+    auto executor = coro_io::get_global_block_executor();
+    for (int i = 0, lim = thread_cnt; i < lim; ++i) {
+      send()
+      .via(coro_io::get_global_executor())
+      .start([executor](auto&& res){
+        executor->schedule([res = std::move(res.value())]() mutable {
+          result.insert(result.end(), res.begin(), res.end());
+          --working_echo;
+        });
+      });
+    }
+    co_await qps_watcher();
+    latency_watcher();
+    co_return;
+  }
+
   Lazy<void> test() {
     co_await test_rpc();
     co_await test_rpc1();
+    co_await test_rpc2();
+    
     get_app()->stop();
     co_return;
   }
